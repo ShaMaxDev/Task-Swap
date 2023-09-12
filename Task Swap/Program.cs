9d8abd487f87f64fe5b9b905f2743f7efd0fc27b
ShaMaxDev <maksimbychkov@yahoo.com>
2023-09-11 21:06:51 -0700

    Add project files.

---
+++ Task Swap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bychkov";
+            string lastName = "Max";
+            string buffer;
+            Console.WriteLine("Before correcting Name is " + name + " and last name is " + lastName);
+            buffer = name;
+            name = lastName;
+            lastName = buffer;
+            Console.WriteLine("After correcting Name is " + name + " and last name is " + lastName);
+
+        }
+    }
+}
